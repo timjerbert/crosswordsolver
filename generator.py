ab1523bf5e7f2eb3c376cfa37c62c548b66af33e
maniamoth <timjsherbert@gmail.com>
2026-06-23 01:40:46 -0500

    Ensuring git working on new OS and aded PyQt5

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from PyQt5.QtWidgets import QApplication
 
 def getPossibleLetterCombinations():
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
